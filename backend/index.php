98856b35cb9fdd1f583b5dcd6e252f506509aaca
delta107 <hhuyy107@gmail.com>
2025-11-24 13:59:03 +0700

    Deploy test

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,11 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $ten = $_POST["hoten"] ?? "Không có dữ liệu";
-    echo "Xin chào: " . htmlspecialchars($ten);
-} else {
-    echo "Vui lòng gửi dữ liệu bằng POST!";
+header("Content-Type: text/plain; charset=utf-8");
+
+if (!isset($_POST['fullname'])) {
+    echo "Không nhận được dữ liệu!";
+    exit;
 }
-?>
+
+$name = htmlspecialchars($_POST['fullname']);
+
+echo "Xin chào $name! Backend (PHP) đã nhận được dữ liệu từ Frontend.";
